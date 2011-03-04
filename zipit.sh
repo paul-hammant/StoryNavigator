d462797f0620e279de4f4dc5a8be7e3678d7a649
Paul Hammant <paul@hammant.org>
2011-03-03 21:59:05 -0600

    fix path

--- zipit.sh
+++ zipit.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-jar -cvfM StoryNavigator-1.0.zip js style/navigator.css style/jquery-ui.css images StoryNavigator.html 
+jar -cvfM StoryNavigator-1.0.zip js style/navigator.css style/jquery-ui.css images navigator.html 
